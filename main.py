a23b3b8141910a5f4d2fc0b321788efa29651d8e
TSOlami <tijani.saheed@yahoo.com>
2024-07-02 16:41:53 +0100

    chore: Add voice command functionality

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# main.py
+# Importing the necessary libraries
+
+
+# Listen to the user's voice command
+
+# Recognize the user's voice command
+
+# Perform the necessary action based on the user's voice command
