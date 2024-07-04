80b02ee72a03028ee9628c1ca0aa083c714330a0
TSOlami <tijani.saheed@yahoo.com>
2024-07-04 11:09:39 +0100

    chore: Add trigger word detection functionality to main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from modules.speech_recognition import recognize_speech_from_microphone
-# from modules.trigger_word_detection import detect_trigger_word
+from modules.trigger_word_detection import listen_for_trigger_word
+from modules.google_recog import recognize_speech_from_microphone
+# from modules.speech_recognition import listen_for_command
 # from modules.command_processing import process_command
 # from modules.text_to_speech import speak_text
 # from modules.model_interaction import get_response_from_model
 
 def main():
-    print("Listening for trigger word...")
+    print("Starting...")
     while True:
-        text = recognize_speech_from_microphone()
-        # if detect_trigger_word(text):
-        #     print("Trigger word detected. Listening for command...")
-        #     command = recognize_speech_from_microphone()
-        #     if command:
+        if listen_for_trigger_word():
+            print("Trigger word detected. Listening for command...")
+            command = recognize_speech_from_microphone()
+            if command:
+                print(command)
         #         processed_command = process_command(command)
         #         response = get_response_from_model(processed_command)
         #         speak_text(response)
