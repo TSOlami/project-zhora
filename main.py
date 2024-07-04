5ee77d9fc6b3d3df27d771b82c78adf927be6af9
TSOlami <tijani.saheed@yahoo.com>
2024-07-04 15:34:45 +0100

    chore: Update main.py to include command processing and model interaction

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from modules.trigger_word_detection import listen_for_trigger_word
 from modules.google_recog import recognize_speech_from_microphone
 # from modules.speech_recognition import listen_for_command
-# from modules.command_processing import process_command
+from modules.command_processing import process_command
 # from modules.text_to_speech import speak_text
-# from modules.model_interaction import get_response_from_model
+from modules.model_interaction import get_response_from_model
 
 def main():
     print("Starting...")
@@ -13,8 +13,9 @@ def main():
             command = recognize_speech_from_microphone()
             if command:
                 print(command)
-        #         processed_command = process_command(command)
-        #         response = get_response_from_model(processed_command)
+                processed_command = process_command(command)
+                response = get_response_from_model(processed_command)
+                print(response)
         #         speak_text(response)
 
 if __name__ == "__main__":
