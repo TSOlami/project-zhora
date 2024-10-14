d8f046fbd29d6626860a8f5b1afe110b6d7a504e
TSOlami <tijani.saheed@yahoo.com>
2024-10-14 16:40:20 +0100

    Refactor keyword path in create_porcupine_instance function

--- models/create_model_instance.py
+++ models/create_model_instance.py
@@ -5,7 +5,7 @@ from config import PICOVOICE_ACCESS_KEY
 
 def create_porcupine_instance():
     access_key = PICOVOICE_ACCESS_KEY
-    keyword_path = r"C:\Users\teejay\www\my projects\project-zhora\models\porcupine\Hey-Zora_en_windows_v3_0_0\Hey-Zora_en_windows_v3_0_0.ppn"
+    keyword_path = r"/home/teejay/www/personal/project-zhora/models/porcupine/Hey-Zora_en_linux_v3_0_0.ppn"
     return pvporcupine.create(access_key=access_key, keyword_paths=[keyword_path])
 
 def create_leopard_instance():
